db1a91705a14dc4e08ec386e9d73a9f6d99a658b
Michael Siddall <michaelsiddall@Michaels-MacBook-Pro.local>
2020-08-28 21:55:59 -0500

    added input form in index and addEmployee func

--- client.js
+++ client.js
@@ -1,5 +1,21 @@
-console.log("in JS");
-
 $(document).ready(onReady);
 
 let employee = [{}];
+
+console.log(employee);
+
+function addEmployee() {
+  console.log("in addEmployee");
+  // get user ipur & place into new object
+  const newEmployee = {
+    firstName: $("#firstNameIn").val(),
+    lastName: $("#lastNameIn").val(),
+    idNumber: $("#idNumberIn").val(),
+    title: $("#titleIn").val(),
+    annualSalary: $("#annualSalaryIn").val(),
+  };
+}
+
+function onReady() {
+  console.log("Lets go!");
+}
