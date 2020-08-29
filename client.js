d805e0c4ad2b489da868e121dae4fc1c56fef52a
Michael Siddall <michaelsiddall@Michaels-MacBook-Pro.local>
2020-08-28 22:14:50 -0500

    working on submit button

--- client.js
+++ client.js
@@ -5,17 +5,23 @@ let employee = [{}];
 console.log(employee);
 
 function addEmployee() {
-  console.log("in addEmployee");
-  // get user ipur & place into new object
+  console.log("in addEmployee", addEmployee);
+  // get user input & place into new object
   const newEmployee = {
     firstName: $("#firstNameIn").val(),
     lastName: $("#lastNameIn").val(),
     idNumber: $("#idNumberIn").val(),
     title: $("#titleIn").val(),
     annualSalary: $("#annualSalaryIn").val(),
-  };
-}
+  }; // end new item
+  //push new object into our array
+  employee.push(newEmployee);
+  //display employees
+} // end addEmployee
 
 function onReady() {
   console.log("Lets go!");
+  //capture click event on
+  $(document).on("click", "addEmployeeBtn", addEmployee);
+  console.log("clicked button");
 }
