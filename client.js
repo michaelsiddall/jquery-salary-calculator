5a204e507b9a2fe55c06d4443e3282374cf9466d
Michael Siddall <michaelsiddall@Michaels-MacBook-Pro.local>
2020-08-29 08:03:57 -0500

    working on Monthly Cost func

--- client.js
+++ client.js
@@ -1,11 +1,10 @@
 $(document).ready(onReady);
 
-let employee = [{}];
+let employee = [];
 
 console.log(employee);
 
 function addEmployee() {
-  console.log("in addEmployee", addEmployee);
   // get user input & place into new object
   const newEmployee = {
     firstName: $("#firstNameIn").val(),
@@ -16,12 +15,41 @@ function addEmployee() {
   }; // end new item
   //push new object into our array
   employee.push(newEmployee);
+  displayEmployee();
   //display employees
 } // end addEmployee
 
 function onReady() {
   console.log("Lets go!");
-  //capture click event on
-  $(document).on("click", "addEmployeeBtn", addEmployee);
-  console.log("clicked button");
+  //capture click event
+
+  $("#addEmployeeBtn").on("click", addEmployee);
+  console.log("clicked button", $(this));
+}
+
+function displayEmployee() {
+  console.log("in displayEmployee");
+  //target an ul element on DOM
+  let el = $("#employeeOut");
+  // empty el
+  el.empty();
+  // loop through array
+  for (let i = 0; i < employee.length; i++) {
+    // append each item to the ul
+    el.append(
+      `<li>${employee[i].firstName} ${employee[i].lastName}, ${employee[i].idNumber}, ${employee[i].title}, ${employee[i].annualSalary}</li>`
+    );
+  }
+}
+
+function monthlyCosts(employee) {
+  let totalAnnualSalary = 0;
+  // loop through employee array
+  for (let i = 0; i <employee.length, i++) {
+    // for all employee, add up total annual salary
+    totalAnnualSalary = totalAnnualSalary += Number(employee[i].annualSalary);
+    // determine monthly salary for each employee
+    let totalMonthlySalary = totalAnnualSalary / 12;
+    
+  }
 }
